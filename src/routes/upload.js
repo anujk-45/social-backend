8044a64c30176d2464f410914c55c33186ba159b
anujk-45 <anujkumarjaimini025@gmail.com>
2022-07-29 15:36:19 +0530

    some changes

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -2,8 +2,7 @@ const upload = require('../middleware/upload');
 const express = require('express');
 const router = express.Router();
 
-// const API = process.env.API;
-const API = 'http://localhost:8000/';
+const API = process.env.API;
 
 router.post('/upload', upload.single('file'), async (req, res) => {
   if (req.file === undefined) return res.send('you must select a file.');
